052dc45fdf2d857a2b84151d93b281fad3bc9460
idomshi <56523771+idomshi@users.noreply.github.com>
2021-12-18 22:03:45 +0900

    とりあえずトップページに載せられそうな文字を打ち込みました。

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,14 @@
 <template>
-<h1>みが市役所広報課だより</h1>
+<header>
+    <h1>みが市役所広報課だより</h1>
+    <p>せつめい</p>
+</header>
+<main>
+    <ul>
+        <li>ギャラリー</li>
+    </ul>
+</main>
+<footer>
+    <p>produced by みが市 members</p>
+</footer>
 </template>
